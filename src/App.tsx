71beaa45af7ca809b43e36702e3237998ebe4c28
deniskrastev9-cpu <deniskrastev9@gmail.com>
2026-05-13 19:27:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-function Home() {
+export default function App() {
   return (
     <div style={{ padding: 40 }}>
       <h1>DVS Automation Solutions</h1>
@@ -13,42 +13,4 @@ function Home() {
       </p>
     </div>
   );
-}
-
-function Privacy() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Privacy Policy</h1>
-
-      <p>
-        We collect contact information only to provide communication services.
-        We do not sell personal data.
-      </p>
-
-      <p>
-        SMS consent required. Reply STOP to opt out.
-      </p>
-
-      <a href="/">Back to Home</a>
-    </div>
-  );
-}
-
-function Terms() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Terms of Service</h1>
-
-      <p>
-        Services must be used for lawful business purposes only.
-        Spam or abuse is prohibited.
-      </p>
-
-      <a href="/">Back to Home</a>
-    </div>
-  );
-}
-
-export default function App() {
-  return <Home />;
 }
